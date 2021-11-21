4a1a888eae5109b1f184ebc5e0c50b71fcf266fd
Leif Gehrmann <leif.gehrmann@gmail.com>
2021-11-21 00:01:34 +0000

    Implemented acceptance tests that get ~92% coverage

--- cypress/integration/imageCompare.spec.ts
+++ cypress/integration/imageCompare.spec.ts
@@ -1,17 +1,169 @@
-describe('image-compare', () => {
-  beforeEach(() => {
-    const host = Cypress.env('IMAGE_COMPARE_HOST');
-    if (typeof host !== 'string' || host === '') {
-      throw new Error('cypress environment variable IMAGE_COMPARE_HOST is not set');
-    }
-    const configUrl = 'Todo'; // Todo
-    cy.viewport(375, 800); // Roughly the size of an iPhone X.
-    cy.visit(`${host}#${configUrl}`);
-  });
+function getHost(): string {
+  const host = Cypress.env('IMAGE_COMPARE_HOST');
+  if (typeof host !== 'string' || host === '') {
+    throw new Error('cypress environment variable IMAGE_COMPARE_HOST is not set');
+  }
+  return host;
+}
 
-  describe('Todo', () => {
-    it('Todo', () => {
-      // Todo:
+function getConfigUrl(): string {
+  const host = getHost();
+  if (host.indexOf('3000') !== -1) {
+    return `${host}example/config-local-3000.json`;
+  }
+  if (host.indexOf('5000') !== -1) {
+    return `${host}example/config-local-5000.json`;
+  }
+  // Sorry for the inconvenience if you run into this :(
+  throw new Error('Only localhost ports 3000 and 5000 are supported for running tests.');
+}
+
+function getHostUrlWithConfig(config: string): string {
+  const host = getHost();
+  return `${host}#${config}`;
+}
+
+function getFullImagePath(image: string): string {
+  const host = getHost();
+  return `${host}${image}`;
+}
+
+function resolveElement(elements: JQuery): HTMLElement {
+  if (elements.length !== 1) {
+    throw new Error('Unexpected number of elements found');
+  }
+  return elements[0];
+}
+
+function touchSwipe(
+  destination: {x: number, y: number},
+  selector: string,
+  callback: () => void,
+  cancel = false,
+) {
+  cy.get(selector).then((elements) => {
+    const element = resolveElement(elements);
+    const currentTouches = [{
+      clientX: element.getBoundingClientRect().x,
+      clientY: element.getBoundingClientRect().y,
+    }];
+    const newTouches = [{
+      clientX: destination.x,
+      clientY: destination.y,
+    }];
+    cy.get(selector)
+      .trigger('touchstart', {
+        touches: currentTouches,
+        changedTouches: currentTouches,
+      })
+      // Simulate touchmove with an empty touches list (for code-coverage sake)
+      .trigger('touchmove', { touches: [] })
+      .trigger('touchmove', {
+        touches: newTouches,
+        changedTouches: newTouches,
+      })
+      .trigger(cancel ? 'touchcancel' : 'touchend', {
+        touches: newTouches,
+        changedTouches: newTouches,
+      });
+    callback();
+  });
+}
+
+const viewportWidth = 375;
+const viewportHeight = 800;
+
+describe('image-compare', () => {
+  describe('configuration loads successfully', () => {
+    beforeEach(() => {
+      cy.viewport(viewportWidth, viewportHeight); // Roughly the size of an iPhone X.
+      cy.visit(getHostUrlWithConfig(getConfigUrl()));
+    });
+
+    describe('segmented controls', () => {
+      it('shows four buttons in the correct selection with the correct image', () => {
+        cy.get('button').should('have.length', 4);
+        cy.get('button').eq(0).should('have.text', 'Dark Variant');
+        cy.get('button').eq(1).should('have.text', 'Light Variant');
+        cy.get('button span.opacity-100').should('have.length', 1);
+        cy.get('button span.opacity-70').should('have.length', 3);
+        const imageUrl = getFullImagePath('example/dark.png');
+        cy.get(`img[src="${imageUrl}"]`).should('have.length', 1);
+      });
+      describe('mouse events', () => {
+        it('clicking the buttons will change the selection', () => {
+          cy.contains('Light Variant').click();
+          const imageUrl = getFullImagePath('example/light.png');
+          cy.get(`img[src="${imageUrl}"]`).should('have.length', 1);
+          cy.contains('Dark Variant').click();
+        });
+      });
+      describe('keyboard events', () => {
+        it('pressing left/right will move the selection left/right respectively', () => {
+          cy.contains('Light Variant').focus().type('{rightarrow}');
+          let imageUrl = getFullImagePath('example/light.png');
+          cy.get(`img[src="${imageUrl}"]`).should('have.length', 1);
+          cy.contains('Light Variant').focus().type('{leftarrow}');
+          imageUrl = getFullImagePath('example/dark.png');
+          cy.get(`img[src="${imageUrl}"]`).should('have.length', 1);
+          cy.contains('Dark Variant').click();
+        });
+      });
+      describe('touch events', () => {
+        it('changes selection when touch-dragging along the control surface starting from the selected item and leaving offscreen', () => {
+          touchSwipe(
+            { x: (viewportWidth / 4) * 2.5, y: viewportHeight / 2 },
+            'button:nth-child(1)',
+            () => {
+              const imageUrl = getFullImagePath('example/first-republic.png');
+              cy.get(`img[src="${imageUrl}"]`).should('have.length', 1);
+              cy.contains('Dark Variant').click();
+            },
+          );
+        });
+        it('does change selection when touch-dragging along the control surface starting from an unselected item and leaving on another item', () => {
+          touchSwipe(
+            { x: (viewportWidth / 4) * 2.5, y: viewportHeight - 40 },
+            'button:nth-of-type(2)',
+            () => {
+              const imageUrl = getFullImagePath('example/first-republic.png');
+              cy.get(`img[src="${imageUrl}"]`).should('have.length', 1);
+              cy.contains('Dark Variant').click();
+            },
+          );
+        });
+        it('does not change selection when touch-dragging beyond control surface and releasing', () => {
+          touchSwipe(
+            { x: viewportWidth - 5, y: viewportHeight / 2 },
+            'button:nth-child(1)',
+            () => {
+              const imageUrl = getFullImagePath('example/dark.png');
+              cy.get(`img[src="${imageUrl}"]`).should('have.length', 1);
+              cy.contains('Dark Variant').click();
+            },
+          );
+        });
+        it('does not change selection when touch-dragging directly in middle of control surface', () => {
+          touchSwipe(
+            { x: viewportWidth / 2, y: viewportHeight / 2 },
+            'button:nth-child(1)',
+            () => {
+              const imageUrl = getFullImagePath('example/dark.png');
+              cy.get(`img[src="${imageUrl}"]`).should('have.length', 1);
+              cy.contains('Dark Variant').click();
+            },
+          );
+        });
+      });
+    });
+  });
+  describe('configuration load failures', () => {
+    beforeEach(() => {
+      cy.viewport(viewportWidth, viewportHeight); // Roughly the size of an iPhone X.
+      cy.visit(getHostUrlWithConfig('error'));
+    });
+    it('displays a message saying the configuration could not be loaded', () => {
+      cy.contains('Failed to load configuration').should('have.length', 1);
     });
   });
 });
